7bc6f3e74e721937ea7fa720cd346919058e5192
Youcef-Goudjal <m.goudjal.y@gmail.com>
2022-10-07 09:53:34 +0100

    assets svg

--- lib/application_layer/invoices/pages/invoices_page.dart
+++ lib/application_layer/invoices/pages/invoices_page.dart
@@ -1,12 +1,75 @@
+import 'package:dzpos/core/extensions/extensions.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class InvoicesPage extends StatelessWidget {
   const InvoicesPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: Colors.amber,
+    return Column(
+      children: [
+        AspectRatio(
+          aspectRatio: 2,
+          child: Row(
+            children: List.generate(
+              2,
+              (index) => ElevatedButton(
+                onPressed: () {},
+                child: Text(Invoices.values[index].name),
+              ),
+            ),
+          ),
+        ),
+        20.h.heightBox,
+        AspectRatio(
+          aspectRatio: 2,
+          child: Row(
+            children: List.generate(
+              2,
+              (index) => ElevatedButton(
+                onPressed: () {},
+                child: Text(Invoices.values[index].name),
+              ),
+            ),
+          ),
+        ),
+        20.h.heightBox,
+        ListView.separated(
+          itemCount: Invoices.values.length - 4,
+          itemBuilder: (context, index) {
+            return TextButton(
+              onPressed: () {},
+              child: Text(Invoices.values[index + 4].name),
+            );
+          },
+          separatorBuilder: (context, index) {
+            return 20.h.heightBox;
+          },
+        ),
+      ],
     );
   }
+}
+
+enum Invoices {
+  newSaleInvoice("New Sale Invoice"),
+  newPurchaseInvoice("New Purchase Invoice"),
+  storeInventory("Store Inventory"),
+  returnedDamaged("Returned-Damaged"),
+  showInvoices("Show Invoices"),
+  totalDailyInvoicesReports("Total Daily Invoices Reports"),
+  activityOfTheMaterial("Activity Of The Material"),
+  editPrices("Edit Prices"),
+  pricingPolicy("Pricing Policy"),
+  inventoryInvoice("Inventory Invoice"),
+  storeReconciliationAndRepair("Store Reconciliation And Repair"),
+  listOfMaterials("List Of Materials"),
+  barCodeDesigner("Barcode Designer");
+
+  final String name;
+
+  const Invoices(this.name);
+
+  void onPressed(BuildContext context) {}
 }
--- lib/application_layer/reports/pages/reports_page.dart
+++ lib/application_layer/reports/pages/reports_page.dart
@@ -1,12 +1,28 @@
 import 'package:flutter/material.dart';
 
 class ReportsPage extends StatelessWidget {
-  const ReportsPage ({Key? key}) : super(key: key);
+  const ReportsPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-   return Container(
+    return Container(
       color: Colors.green,
     );
   }
+}
+
+enum Reports {
+  profitLoss("Profit-Loss"),
+  dailyTransaction("Daily Transaction"),
+  budget("Capital-Budget"),
+  theMostSoldMaterials("The most sold materials"),
+  theMostActiveCustomers("The most Active Customers"),
+  showItemsThatHaveReachedAMinimum("Show Items that have reached a minimum"),
+  displayItemsWhoseQuantityIsLessThan(
+      "Display Items whose quantity is less than"),
+  lessMovingMaterials("Less moving materials"),
+  lessMobileClients("Less mobile clients");
+
+  final String name;
+  const Reports(this.name);
 }
